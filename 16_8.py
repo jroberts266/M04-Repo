d2507a04783eac07aabe5b1444f98bd808ec7b84
jroberts266 <123031370+jroberts266@users.noreply.github.com>
2023-02-12 21:50:51 -0500

    Update 16_8.py

--- 16_8.py
+++ 16_8.py
@@ -1,3 +1,9 @@
+#16.8 assingment
+#Author: Joey Roberts
+#Date: 2/12/2023
+# Using SQLAlchemy
+
+
 import sqlalchemy
 conn = sqlalchemy.create_engine('sqlite:///books.db')
 sql = 'select title from book order by title asc'
