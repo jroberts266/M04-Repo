feb42f8312e196ad2f7d8c46a2e900a2260b025e
jroberts266 <123031370+jroberts266@users.noreply.github.com>
2023-02-12 21:53:03 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
+#Chapter 11 assignment
+#Author: Joey Roberts
+#Date: 2/12/2023
+#Imports function from another file
+
+#imports zoo function
 import zoo 
+
 hours = zoo.hours()
 print("The hours are", hours)
 
+#imports zoo as a new name
 import zoo as menagerie
 hours = menagerie.hours()
 print(hours)
